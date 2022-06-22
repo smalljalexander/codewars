7cc560ad143c869ef927e59f5c800680502ea2e5
Alex Small <smalljalexander@gmail.com>
2022-06-21 20:47:31 -0400

    completed first try

---
+++ js/solution038.js
@@ -0,0 +1,17 @@
+//Complete the solution so that it reverses the string passed into it.
+
+//'world'  =>  'dlrow'
+//'word'   =>  'drow'
+
+
+//Parameters- str-take in a string of one word
+//return- reverse the order of letters in the string
+//examples- 'world' => 'dlrow', word => drow, dog => god
+//psuedocode- take in a string of one word, reverse the order of the letters return it
+
+
+
+
+function solution(str){
+    return str.split('').reverse('').join('')
+}
