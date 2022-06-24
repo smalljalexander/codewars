abf91a453530ad6dcf1d5cfe65f6bd1037eb64d9
Alex Small <smalljalexander@gmail.com>
2022-06-24 10:04:45 -0400

    completed

---
+++ js/solution042.js
@@ -0,0 +1,14 @@
+// parameters-takes in a single number 
+// return- a string based on the number 
+// examples- 10 => great now move onto tricks, 11=> great now move onto tricks, 9 => keep at it until you get it 
+// psuedocode- take in a number. else if statment  for n return it 
+
+
+
+function hoopCount (n) {
+    if(n >= 10){
+        return 'Great, now move on to tricks'
+ }   else{
+         return 'Keep at it until you get it'
+ }
+ }
