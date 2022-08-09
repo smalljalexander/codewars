bb46d7a46b860d59c4a089805d5c11f4122c8d8d
Alexander Small <103280475+smalljalexander@users.noreply.github.com>
2022-08-08 20:35:34 -0400

    First try!

---
+++ js/solution079.js
@@ -0,0 +1,11 @@
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+// take in a number
+// return the square of number 
+// 3 => 9, 6 => 36, -5 => 25
+// return the number multiplied by itself.
+
+function square(n){
+     return n * n
+}
