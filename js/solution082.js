d48086ccde9f60ad423e15e5a7f960f59517324f
Alexander Small <103280475+smalljalexander@users.noreply.github.com>
2022-08-11 09:22:25 -0400

    First try!

---
+++ js/solution082.js
@@ -0,0 +1,15 @@
+//take in a number that is negative or positive
+//return the opposite of that number
+//multiply the number by -1 to get the opposite of the number
+
+//Very simple, given an integer or a floating-point number, find its opposite.
+
+//Examples:
+//1: -1
+//14: -14
+//-34: 34
+
+
+function opposite(number) {
+  return number * -1
+}
