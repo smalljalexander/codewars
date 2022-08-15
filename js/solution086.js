9987e2c06751da09405452cac0fa2bb602f78d7c
Alexander Small <103280475+smalljalexander@users.noreply.github.com>
2022-08-15 08:05:29 -0400

    Completed

---
+++ js/solution086.js
@@ -0,0 +1,16 @@
+//Count the number of divisors of a positive integer n.
+//Random tests go up to n = 500000.
+//Examples (input --> output)
+//4 --> 3 (1, 2, 4)
+//5 --> 2 (1, 5)
+//12 --> 6 (1, 2, 3, 4, 6, 12)
+//30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+
+
+function getDivisorsCnt(n) {
+  for (var d = 0, i = n; i > 0; i--) {
+    if (n % i == 0) d++;
+  }
+  
+  return d;
+}
