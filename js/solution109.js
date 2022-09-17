58af4b1a056968a43ae85f3a887444c0a1e9f592
Alexander Small <103280475+smalljalexander@users.noreply.github.com>
2022-09-17 11:06:09 -0400

    First try!!

---
+++ js/solution109.js
@@ -0,0 +1,15 @@
+//You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+//Array can contain numbers or strings. X can be either.
+//Return true if the array contains the value, false if not.
+
+
+
+
+// take in an array and a value in separate variables 
+// return true if the letter or number is in the array and false if not 
+// use the includes() method to return true  if value is in array and of false if not
+
+
+function check(a, x) {
+  return a.includes(x)
+}
